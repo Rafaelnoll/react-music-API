b5f2de1ff44d139701bbca89fbb1c74e62ba9ee6
Rafael Noll <rafanoll2004@gmail.com>
2023-01-06 16:47:14 -0300

    Adding new ambient variables to connect with the database

--- database/firebaseConnection.js
+++ database/firebaseConnection.js
@@ -1,8 +1,19 @@
 const { initializeApp } = require("firebase/app");
 const { getAuth } = require("firebase/auth");
-const { getFirestore } = require("firebase/firestore");
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceFireStore.json");
+
+const serviceAccount = {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECTID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
